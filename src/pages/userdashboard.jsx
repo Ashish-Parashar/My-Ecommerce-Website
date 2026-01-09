152600d0a488f791fc2d7e6dc56c89dca78aa567
Ashish-Parashar <ashishparashar525@gmail.com>
2026-01-09 19:38:14 +0530

    Update userdashboard.jsx

--- src/pages/userdashboard.jsx
+++ src/pages/userdashboard.jsx
@@ -15,7 +15,12 @@ import banner3 from '../assets/still-life-with-classic-shirts_23-2150828634.jpg'
 
 function Userdashboard() {
     const [products, setproduct] = useState([])
+    const [Productcategory, setProductcategory] = useState('All')
+    const [Catacitve, setCatactive] = useState(0)
+
+
     const ProductsUrl = 'https://backend-jvcj.onrender.com/api/products';
+
 
     const dispatch = useDispatch()
 
@@ -46,7 +51,11 @@ function Userdashboard() {
         banner2,
         banner3
     ];
-    
+
+    const categoryName = ['All', ...new Set(products.map((item) => item.category.name ? item.category.name: item.category))]
+
+    const filtercat = Productcategory === 'All' ? products :
+        products.filter((item) => item.category.name === Productcategory)
 
     return (
         <>
@@ -58,7 +67,6 @@ function Userdashboard() {
                     delay: 10000,
                     disableOnInteraction: false,
                 }}
-                // pagination={{ clickable: true }}
                 navigation={true}
                 loop={true}
                 className="mySwiper"
@@ -69,10 +77,8 @@ function Userdashboard() {
                             className="relative w-full h-[350px]  md:h-[500px] bg-cover bg-center flex items-center justify-center text-center text-white"
                             style={{ backgroundImage: `url(${img})` }}
                         >
-                            {/* Dark overlay for better text contrast */}
                             <div className="absolute inset-0 bg-black/40"></div>
 
-                            {/* Text content */}
                             <div className="relative z-10 max-w-2xl px-6">
                                 <h2 className="text-3xl md:text-5xl font-bold tracking-wide mb-4 drop-shadow-lg">
                                     Elevate Your Style
@@ -80,22 +86,37 @@ function Userdashboard() {
                                 <p className="text-sm md:text-lg font-light mb-6">
                                     Discover the latest trends in men’s fashion — be bold, be confident.
                                 </p>
-                                <button className="bg-white text-gray-900 font-semibold px-6 py-2 rounded-full hover:bg-gray-200 transition-all">
-                                    Shop Now
-                                </button>
+
                             </div>
                         </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
 
-          
+
+
+            <div className=' bg-white dark:bg-gray-800 flex flex-wrap gap-3 py-4 p-2' >
+                {
+                    categoryName.map((item, index) =>
+                        <button className="px-4 py-2 rounded-md bg-blue-500  text-white hover:bg-blue-700 transition-all duration-200 text-sm font-semibold shadow-sm"
+                            style={{
+                                backgroundColor: Catacitve === index ? '#1D4ED8' : null
+                            }}
+                            onClick={() => {
+                                setProductcategory(item)
+                                setCatactive(index)
+                            }}
+                        >{item}</button>
+                    )
+                }
+            </div>
+
             <div className="p-4  sm:p-6 lg:p-8 max-w-[1340px] mx-auto">
-                {products.length === 0 ? (
+                {filtercat.length === 0 ? (
                     <p className="text-center text-gray-500 text-lg">No products available.</p>
                 ) : (
                     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-6">
-                        {products.map((product) => (
+                        {filtercat.map((product) => (
                             <div
                                 key={product._id}
                                 className="bg-white rounded-2xl shadow-md hover:shadow-xl transition-all duration-300 overflow-hidden flex flex-col group"
